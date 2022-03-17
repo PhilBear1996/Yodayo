bf86c85d9e0d79fc7f7f32ffe708df0658ba2689
zxcv1230174 <101238949+zxcv1230174@users.noreply.github.com>
2022-03-17 12:34:01 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@
 #include<ctime>
 #include<cstdlib>
 void blockT
+class Tetris
+{
+  private:
+    
+}
+class Move
+{
+  private:
+    
+}
+class Eliminate
+{
+  private:
+    
+}
 using namespace std;
 int main{
   
